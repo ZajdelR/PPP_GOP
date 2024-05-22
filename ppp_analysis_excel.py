563285be2d247d76ccb11f3fc4b912e5653be597
Radek <radoslaw.zajdel@upwr.edu.pl>
2024-05-22 10:34:32 +0200

    changed input

--- ppp_analysis_excel.py
+++ ppp_analysis_excel.py
@@ -4,7 +4,7 @@ import os
 
 # Main execution
 if __name__ == "__main__":
-    filepath = './INPUT/STATISTICS_v1'
+    filepath = './INPUT/STATISTICS_v3'
     data = pppu.load_and_prepare_data(filepath)
     analysis_results = pppu.analyze_by_ac_and_site(data)
     
