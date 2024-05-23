b75218f2661b214b8579c93971f825fa90445b8f
ZajdelR <radoslaw.zajdel@upwr.edu.pl>
2024-05-23 10:37:45 +0200

    PEP changes to PPP_utils

--- ppp_analysis_plot_heatmap.py
+++ ppp_analysis_plot_heatmap.py
@@ -6,30 +6,31 @@ import os
 if __name__ == "__main__":
     filepath = './INPUT/STATISTICS_v3'
     data = pppu.load_and_prepare_data(filepath)
-    
+    systems = data.Systems.unique()
+
     vlim = (80,100)    
-    ratGRE = pppu.heatmap_station_specific(data,'30S','GREx','Rat',scalar=1)
-    ratGR = pppu.heatmap_station_specific(data,'30S','GRxx','Rat',scalar=1)
-    ratG = pppu.heatmap_station_specific(data,'30S','Gxxx','Rat',scalar=1)
+    ratGRE = pppu.create_heatmap_station_specific(data,'30S','GREx','Rat',scalar=1)
+    ratGR = pppu.create_heatmap_station_specific(data,'30S','GRxx','Rat',scalar=1)
+    ratG = pppu.create_heatmap_station_specific(data,'30S','Gxxx','Rat',scalar=1)
     
     stat = 'Vpe95'
     
     vlim = (10,100)
-    pppu.heatmap_station_specific(data,'30S','GREx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
-    pppu.heatmap_station_specific(data,'30S','GRxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
-    pppu.heatmap_station_specific(data,'30S','Gxxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
+    pppu.create_heatmap_station_specific(data,'30S','GREx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
+    pppu.create_heatmap_station_specific(data,'30S','GRxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
+    pppu.create_heatmap_station_specific(data,'30S','Gxxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
     
     vlim = (-30,30)
-    pppu.heatmap_station_specific(data,'30S','GREx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
-    pppu.heatmap_station_specific(data,'30S','GRxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
-    pppu.heatmap_station_specific(data,'30S','Gxxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
+    pppu.create_heatmap_station_specific(data,'30S','GREx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
+    pppu.create_heatmap_station_specific(data,'30S','GRxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
+    pppu.create_heatmap_station_specific(data,'30S','Gxxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
     # 
     vlim = (0,30)
-    pppu.heatmap_station_specific(data,'01D','GREx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
-    pppu.heatmap_station_specific(data,'01D','GRxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
-    pppu.heatmap_station_specific(data,'01D','Gxxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
+    pppu.create_heatmap_station_specific(data,'01D','GREx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
+    pppu.create_heatmap_station_specific(data,'01D','GRxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
+    pppu.create_heatmap_station_specific(data,'01D','Gxxx',stat,rev='_r',vmin=vlim[0],vmax=vlim[1])
     
     vlim = (-30,30)
-    pppu.heatmap_station_specific(data,'01D','GREx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
-    pppu.heatmap_station_specific(data,'01D','GRxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
-    pppu.heatmap_station_specific(data,'01D','Gxxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
+    pppu.create_heatmap_station_specific(data,'01D','GREx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
+    pppu.create_heatmap_station_specific(data,'01D','GRxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
+    pppu.create_heatmap_station_specific(data,'01D','Gxxx',stat,vmin=vlim[0],vmax=vlim[1],perc=True)
